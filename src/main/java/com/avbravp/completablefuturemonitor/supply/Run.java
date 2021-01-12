125681b0937bfcbfbd3046e5ceeae1569719c293
avbravo <avbravo@avbravo>
2021-01-12 15:17:03 -0500

    xp9ing

---
+++ src/main/java/com/avbravp/completablefuturemonitor/supply/Run.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.avbravp.completablefuturemonitor.supply;
+
+import com.avbravp.completablefuturemonitor.completable.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+/**
+ *
+ * @author avbravo
+ */
+public class Run {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        // TODO code application logic here
+       Complete c = new Complete();
+       c.completableFutureSupplyAsync();
+        
+                
+    }
+    
+
+    
+}
